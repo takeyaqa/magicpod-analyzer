414445fae16efa9afbfba0e1c0a3e3599fb99746
Takeshi Kishi <takeya0x86@icloud.com>
2022-08-12 17:27:18 +0900

    Change api domain

--- test/commands/get-batch-runs.test.ts
+++ test/commands/get-batch-runs.test.ts
@@ -3,7 +3,7 @@ import {expect, test} from '@oclif/test'
 
 describe('get-batch-runs', () => {
   test
-  .nock('https://magic-pod.com', api => {
+  .nock('https://app.magicpod.com', api => {
     api.get('/api/v1.0/DummyOrg/DummyPrj/batch-runs/?count=100')
     .reply(200, {organization_name: 'DummyOrg', project_name: 'DummyPrj', batch_runs: []})
   })
