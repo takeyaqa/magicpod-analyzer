3dd9b3dd29105094d07af6d10c72b2eca78b7a52
Takeshi Kishi <takeyaqa@gmail.com>
2025-01-07 20:42:48 +0900

    test: create tests for magicpod config loading (#174)
    * test: create tests for magicpod config loading
    
    * fix: downgrade chai-as-promised and @types/chai-as-promised to compatible versions

---
+++ test/magicpod-config.test.ts
@@ -0,0 +1,60 @@
+import * as chai from 'chai'
+import * as chaiAsPromised from 'chai-as-promised'
+
+import {loadConfig} from '../src/magicpod-config'
+
+chai.use(chaiAsPromised)
+
+const {expect} = chai
+
+describe('magicpod-config', () => {
+  it('loadConfig magicpod_analyzer_test_gcs_bigquery.yaml', async () => {
+    const config = await loadConfig('./test/magicpod_analyzer_test_gcs_bigquery.yaml')
+    expect(config).to.deep.equal({
+      projects: [
+        {
+          organization: 'FakeOrganization',
+          name: 'FakeProject',
+          fullName: 'FakeOrganization/FakeProject',
+        },
+      ],
+      exporter: {
+        bigquery: {
+          project: 'fake-project',
+          dataset: 'fake-dataset',
+          reports: [{name: 'test_report', table: 'test_report'}],
+        },
+      },
+      lastRunStore: {
+        backend: 'gcs',
+        project: 'fake-project',
+        bucket: 'fake-bucket',
+      },
+    })
+  })
+
+  it('loadConfig magicpod_analyzer_test_local_local.yaml', async () => {
+    const config = await loadConfig('./test/magicpod_analyzer_test_local_local.yaml')
+    expect(config).to.deep.equal({
+      projects: [
+        {
+          organization: 'FakeOrganization',
+          name: 'FakeProject',
+          fullName: 'FakeOrganization/FakeProject',
+        },
+      ],
+      exporter: {
+        local: {
+          outputDir: 'output',
+        },
+      },
+      lastRunStore: {
+        backend: 'local',
+      },
+    })
+  })
+
+  it('load Config not found', async () => {
+    await expect(loadConfig('./test/not_found.yaml')).to.be.rejectedWith('ENOENT: no such file or directory')
+  })
+})
